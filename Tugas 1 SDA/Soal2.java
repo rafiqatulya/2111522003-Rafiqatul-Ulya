e343a45313c83cc44a2c7045b31232e64abcfe77
rafiqatulya <100512111+rafiqatulya@users.noreply.github.com>
2022-03-11 22:49:08 +0700

    Add files via upload

---
+++ Tugas 1 SDA/Soal2.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+//Soal 2
+//2. size()
+public class Soal2 {
+    public static void main(String[] args) 
+    {
+    ArrayList<String> nama = new ArrayList<String>();
+    nama.add("u");
+    nama.add("l");
+    nama.add("y");
+    nama.add("a");
+    System.out.println(" ");
+    System.out.printf("Panjang dari array tersebut adalah : ");
+    System.out.println(nama.size());
+    System.out.println(" ");
+    System.out.println(" ");
+
+    }
+    }
